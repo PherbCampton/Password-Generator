7d4ef60dfa20ce2e0510d690ca1d180dbbfa6f0a
Samuel Tuinperi <tuinperisamuel@yahoo.com>
2022-02-25 22:14:12 +0100

    Project completed

---
+++ index.js
@@ -0,0 +1,27 @@
+let pass1El = document.getElementById("password1")
+let pass2El = document.getElementById("password2")
+let pass3El = document.getElementById("password3")
+let pass4El = document.getElementById("password4")
+
+
+function gen() {
+  let srt = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' + 
+  'abcdefghijklmnopqrstuvwxyz0123456789@#$';
+  let pass = ''
+  let size = srt.length
+
+  for (let i = 0; i <= 10; i++) {
+    let char = Math.floor(Math.random() * srt.length + 1)
+    pass += srt.charAt(char)
+  }
+  return pass
+  
+}
+
+
+function generator() {
+  pass1El.value = gen()
+  pass2El.value = gen()
+  pass3El.value = gen()
+  pass4El.value = gen()
+}
